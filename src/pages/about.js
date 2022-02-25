db18e4fe8492c93adbe33cc9a2a668491d573cd2
Diego Aldao <diego_ind@live.com.ar>
2022-02-25 18:52:16 -0300

    añadidas las imagenes, gatsby images, y terminado la pagina about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,33 @@
 import React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/Layout"
 
-export default function about() {
+export default function About() {
   return (
     <Layout>
-      <h2>about</h2>
+      <main className="page">
+        <section className="about-page">
+          <article>
+            <h2>Normcore actually taxidermy, air plant</h2>
+            <p>
+              Man bun readymade woke polaroid crucifix, fixie vexillologist
+              pitchfork hexagon sustainable tumblr raclette portland.
+            </p>
+            <p>
+              Migas squid art party, adaptogen raclette portland woke polaroid
+            </p>
+            <Link to="/contact" className="btn">
+              contacto
+            </Link>
+          </article>
+          <StaticImage
+            src="../assets/images/about.jpeg"
+            alt="persona hechando sal"
+            className="about-img"
+          />
+        </section>
+      </main>
     </Layout>
   )
 }
