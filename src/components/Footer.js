a867484991bc55ec9647efe6c590d2f8ae6b459e
Diego Aldao <diego_ind@live.com.ar>
2022-02-25 16:09:18 -0300

    añadidos estilos al footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,12 @@ import React from "react"
 
 const Footer = () => {
   return (
-    <div>
-      <h2>desde footer</h2>
-    </div>
+    <footer className="page-footer">
+      <p>
+        &copy; {new Date().getFullYear()} <span>RecetasSimples</span> Hecho con{" "}
+        <a href="https://www.gatsbyjs.com/">Gatsby</a>
+      </p>
+    </footer>
   )
 }
 
