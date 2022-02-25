14bc59b6db5dfcef45e683520938c459bfe87bf4
Diego Aldao <diego_ind@live.com.ar>
2022-02-25 19:48:49 -0300

    añadido el hero de la pagina principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,27 @@
 import React from "react"
 import Layout from "../components/layout"
+import { StaticImage } from "gatsby-plugin-image"
 
 export default function Home() {
   return (
     <Layout>
-      <h1>inicio</h1>
+      <main className="page">
+        <header className="hero">
+          <StaticImage
+            src="../assets/images/main.jpeg"
+            alt="eggs"
+            className="hero-img"
+            placeholder="blurred"
+            layout="fullWidth"
+          />
+          <div className="hero-container">
+            <div className="hero-text">
+              <h1>recetas simples</h1>
+              <h4>sin vueltas, solo recetas</h4>
+            </div>
+          </div>
+        </header>
+      </main>
     </Layout>
   )
 }
